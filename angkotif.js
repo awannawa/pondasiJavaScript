bf0b01d7ffe687a332fe3d5e9764735a27a076b6
awannnawa <kurniawanofficiall@gmail.com>
2023-02-09 15:24:25 +0700

    vid 23 materi pengkodisian done

--- angkotif.js
+++ angkotif.js
@@ -1,13 +1,13 @@
 var x = (noAngkot = 1);
-var y = (angkotRusak = 5);
-var v = (lembur = 2);
+var y = (angkotRusak = 4);
+var v = (lembur = 5);
 var z = (jmlAngkot = 10);
 
 for (x; x <= z; x++) {
-  if (x == v) {
-    console.log(`Nomor Angkot ${x} sedang lemburrrr`);
-  } else if (x <= y) {
+  if (x <= y && x !== v) {
     console.log(`Nomor Angkot ${x} sedang Rusak`);
+  } else if (x == v || x == 8 || x == 10) {
+    console.log(`Nomor Angkot ${x} sedang lemburrrr`);
   } else {
     console.log(`Nomor Angkot ${x} beroperasi normal`);
   }
