d7a884eae7ba82226ee253bfa29873d6ea368962
awannnawa <kurniawanofficiall@gmail.com>
2023-02-09 15:09:19 +0700

    vid 22 materi else if done

--- angkotif.js
+++ angkotif.js
@@ -1,11 +1,14 @@
-var noAngkot = 1;
-var jmlAngkot = prompt(`Masukkan jumlah angkot`);
-var angkotRusak = prompt(`Masukkan jumlah angkot rusak`);
+var x = (noAngkot = 1);
+var y = (angkotRusak = 5);
+var v = (lembur = 2);
+var z = (jmlAngkot = 10);
 
-for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
-  if (noAngkot <= angkotRusak) {
-    console.log(`Angkot no ${noAngkot} sedang Rusak`);
+for (x; x <= z; x++) {
+  if (x == v) {
+    console.log(`Nomor Angkot ${x} sedang lemburrrr`);
+  } else if (x <= y) {
+    console.log(`Nomor Angkot ${x} sedang Rusak`);
   } else {
-    console.log(`Angkot no ${noAngkot} sedang beroperasi normal`);
+    console.log(`Nomor Angkot ${x} beroperasi normal`);
   }
 }
