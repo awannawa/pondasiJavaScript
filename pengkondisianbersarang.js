1284f431f27b937fbb0b5cc71a3dd2e992df2b00
awannawa <kurniawanofficiall@gmail.com>
2023-02-10 21:11:35 +0700

    tugas segitiga sama sisi done wit comment selanjutnya bikin diamond

--- pengkondisianbersarang.js
+++ pengkondisianbersarang.js
@@ -1,16 +1,19 @@
 var s = "";
 
 for (i = 0; i <= 10; i++) {
-  //s += "*";
+  //buat nentuin spasi di kiri baris kosongnya
   for (j = 10; j > i; j--) {
     s += " ";
   }
+  // buat baris kiri
   for (k = 0; k < i; k++) {
-    s += "*";
+    s += "1";
   }
+  // buat tengah tengahnya
   s += "*";
+  // buat baris kanan
   for (a = 0; a < i; a++) {
-    s += "*";
+    s += "2";
   }
   s += "\n";
 }
