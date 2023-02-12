ce65ddcaadb6a18cdff3b2912a38c3eb79144e33
awannawa <kurniawanofficiall@gmail.com>
2023-02-12 22:32:21 +0700

    nyoba kombinasi dari openai script buat bikin segitiga pascal tambah tambahan

--- pengkondisianbersarang.js
+++ pengkondisianbersarang.js
@@ -1,32 +1,89 @@
-var s = "";
+function segitigaPascal(n) {
+  var segitiga = [];
+  for (var i = 0; i < n; i++) {
+    segitiga[i] = [];
+    for (var j = 0; j <= i; j++) {
+      if (j === 0 || j === i) {
+        function segitigaPascal(n) {
+          var segitiga = [];
+          for (var i = 0; i < n; i++) {
+            segitiga[i] = [];
+            for (let j = 0; j <= i; j++) {
+              if (j === 0 || j === i) {
+                segitiga[i][j] = 1;
+              } else {
+                segitiga[i][j] = segitiga[i - 1][j - 1] + segitiga[i - 1][j];
+              }
+            }
+          }
+          return segitiga;
+        }
 
-// diamond
-for (i = 10; i > 0; i--) {
-  for (a = 0; a < i; a++) {
-    s += " ";
+        function cetakSegitigaPascal(n) {
+          var segitiga = segitigaPascal(n);
+          for (var i = 0; i < n; i++) {
+            var baris = "";
+            for (var j = 0; j <= i; j++) {
+              baris += segitiga[i][j] + " ";
+            }
+            console.log(baris);
+          }
+        }
+
+        // cetak 10 baris segitiga pascal
+        cetakSegitigaPascal(5);
+        [i][j] = 1;
+      } else {
+        segitiga[i][j] = segitiga[i - 1][j - 1] + segitiga[i - 1][j];
+      }
+    }
   }
-  for (b = 10; b > i; b--) {
-    s += "0";
-  }
-  s += "1";
-  for (c = 10; c > i; c--) {
-    s += "2";
-  }
-  s += "\n";
+  return segitiga;
 }
-for (j = 0; j <= 10; j++) {
-  for (d = 0; d < j; d++) {
-    s += " ";
+
+function printPascalTriangle(n) {
+  let segitiga = generatePascalTriangle(n);
+  for (var i = 0; i < n; i++) {
+    var baris = "";
+    for (var j = 0; j <= i; j++) {
+      baris += segitiga[i][j] + " ";
+    }
+    console.log(baris);
   }
-  for (e = 10; e > j; e--) {
-    s += "0";
-  }
-  s += "5";
-  for (f = 10; f > j; f--) {
-    s += "2";
-  }
-  s += "\n";
 }
+
+// cetak segitiga pascal sebanyak 1x
+segitigaPascal(1);
+
+// var s = "";
+
+// // diamond
+// for (i = 10; i > 0; i--) {
+//   for (a = 0; a < i; a++) {
+//     s += " ";
+//   }
+//   for (b = 10; b > i; b--) {
+//     s += "0";
+//   }
+//   s += "1";
+//   for (c = 10; c > i; c--) {
+//     s += "2";
+//   }
+//   s += "\n";
+// }
+// for (j = 0; j <= 10; j++) {
+//   for (d = 0; d < j; d++) {
+//     s += " ";
+//   }
+//   for (e = 10; e > j; e--) {
+//     s += "0";
+//   }
+//   s += "5";
+//   for (f = 10; f > j; f--) {
+//     s += "2";
+//   }
+//   s += "\n";
+// }
 
 // // segitiga mepet kanan
 // for (i = 10; i > 0; i--) {
@@ -81,4 +138,4 @@ for (j = 0; j <= 10; j++) {
 //   s += "\n";
 // }
 
-console.log(s);
+// console.log(s);
