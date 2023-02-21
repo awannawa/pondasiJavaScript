ebcc4520e422c4e917fb0b4e8fd7f381b32c70c4
awannawa <kurniawanofficiall@gmail.com>
2023-02-21 22:48:36 +0700

    coba code tebak angka dengan js

---
+++ tebakAngka.js
@@ -0,0 +1,9 @@
+//computer cari dengan fungsi Math.Random() dulu di jadiin desimal dari 1-10
+
+// munculkan alert dengan option tebak angka 1-10 dan jumlah maximal percobaan 3x
+
+// muculkan prompt angka tebakan user bila salah 1x kesempatan hilang satu dan dimunculkan pemberitahuan apakah angka yang dimasukkan user terlalu besar atau kecil dst... selama 3x kesempatan...
+
+// bila angka tertebak maka keluar dari loop
+
+// munculkan confirm pop up apakah mau main tebak angka lagi
