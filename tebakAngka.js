522c04173afb59ecc22b7b47b196645e4e0f05d9
awannawa <kurniawanofficiall@gmail.com>
2023-02-25 23:35:21 +0700

    penentuan angka komputer dimasukin ke while nya

--- tebakAngka.js
+++ tebakAngka.js
@@ -1,33 +1,33 @@
 var bermain = true;
-// tanya mau main gak?
-bermain = confirm("Apakah Anda mau bermain tebak angka dengan komputer?");
-//computer cari dengan fungsi Math.Random() dulu di jadiin desimal dari 1-10
-// var k = Math.random();
-// if (k <= 0.1) {
-//   k = "1";
-// } else if (k > 0.1 && k <= 0.2) {
-//   k = "2";
-// } else if (k > 0.2 && k <= 0.3) {
-//   k = "3";
-// } else if (k > 0.3 && k <= 0.4) {
-//   k = "4";
-// } else if (k > 0.4 && k <= 0.5) {
-//   k = "5";
-// } else if (k > 0.5 && k <= 0.6) {
-//   k = "6";
-// } else if (k > 0.6 && k <= 0.7) {
-//   k = "7";
-// } else if (k > 0.7 && k <= 0.8) {
-//   k = "8";
-// } else if (k > 0.8 && k <= 0.9) {
-//   k = "9";
-// } else {
-//   k = "10";
-// }
-var k = Math.floor(Math.random() * 10) + 1;
 
-console.log(`computer memilih : ${k}`);
 while (bermain) {
+  // tanya mau main gak?
+  bermain = confirm("Apakah Anda mau bermain tebak angka dengan komputer?");
+  //computer cari dengan fungsi Math.Random() dulu di jadiin desimal dari 1-10
+  // var k = Math.random();
+  // if (k <= 0.1) {
+  //   k = "1";
+  // } else if (k > 0.1 && k <= 0.2) {
+  //   k = "2";
+  // } else if (k > 0.2 && k <= 0.3) {
+  //   k = "3";
+  // } else if (k > 0.3 && k <= 0.4) {
+  //   k = "4";
+  // } else if (k > 0.4 && k <= 0.5) {
+  //   k = "5";
+  // } else if (k > 0.5 && k <= 0.6) {
+  //   k = "6";
+  // } else if (k > 0.6 && k <= 0.7) {
+  //   k = "7";
+  // } else if (k > 0.7 && k <= 0.8) {
+  //   k = "8";
+  // } else if (k > 0.8 && k <= 0.9) {
+  //   k = "9";
+  // } else {
+  //   k = "10";
+  // }
+  var k = Math.floor(Math.random() * 10) + 1;
+  console.log(`computer memilih : ${k}`);
   const nyawa = 3;
   let i = nyawa;
   // muculkan prompt angka tebakan user bila salah 1x kesempatan hilang satu dan dimunculkan pemberitahuan apakah angka yang dimasukkan user terlalu besar atau kecil dst... selama 3x kesempatan...
@@ -40,7 +40,7 @@ while (bermain) {
     console.log(`Tebakan orang : ${p}`);
     let j = i - 1;
     if (p == k) {
-      alert("Angka yg kamu masukkan : " + p + "\nSelamat Kamu Benar");
+      alert(`Angka yg kamu masukkan : ${p}\nSelamat Kamu Benar`);
       break;
     } else if (p < k) {
       if (j == 0) {
