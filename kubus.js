b045acd0c1e4df1e4a9678392a1faec916e955b3
awannawa <kurniawanofficiall@gmail.com>
2023-02-28 23:01:26 +0700

    funct di dalam funct lanjut pengulangan arguments di dalam fungsi

--- kubus.js
+++ kubus.js
@@ -1,7 +1,18 @@
+// fungsi di dalam fungsi
+
 function kubus(a, b) {
   var totalKubus;
-  totalKubus = a * a + b * b;
+  totalKubus = a * a * a + b * b * b;
 
   return totalKubus;
 }
-alert(kubus(6, 5));
+
+function jumlahDua(a, b) {
+  var jum;
+  jum = a + b;
+  return jum;
+}
+alert(jumlahDua(kubus(6, 5), kubus(7, 8)));
+
+// pengulangan arguments di dalam fungsi
+//??
